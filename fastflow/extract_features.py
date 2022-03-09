52cb04ae532f93a3c2b6148d950719d65146d36f
leonid <leonid>
2022-03-09 18:29:08 +0000

    cash layer output

--- fastflow/extract_features.py
+++ fastflow/extract_features.py
@@ -1,7 +1,27 @@
-
+import torch
+from tqdm import tqdm
+import numpy as np
+import pickle
 from feature_extractor import FeatureExtractor
 
-def create_feature_dataset(model, layers, dataset, out_name):
+
+def create_feature_dataset(model, layers, dataset, out_name, num_images_per_class, device):
     encoder = FeatureExtractor(model, layers)
     encoder.eval()
     
+    print(f'saving to {out_name}...')
+    preds = {layer : [] for layer in layers}
+
+    label_counts = {i:0 for i in range(10)}
+    for image, label in tqdm(dataset):
+        if label_counts[label] > num_images_per_class:
+            continue
+        features = encoder(torch.unsqueeze(image.to(device), dim=0))
+        for layer in features.keys():
+            preds[layer].append(features[layer].detach().cpu())
+        label_counts[label]+=1
+    
+    for layer in preds.keys():
+        preds[layer] = torch.cat(preds[layer]).numpy()
+
+    np.savez_compressed(out_name, **preds)
