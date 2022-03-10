639ca9e6227fd6737bc16d3e51fe1c0969f20ce5
leonid <leonid>
2022-03-10 09:03:24 +0000

    extract_features

--- fastflow/extract_features.py
+++ fastflow/extract_features.py
@@ -5,23 +5,30 @@ import pickle
 from feature_extractor import FeatureExtractor
 
 
-def create_feature_dataset(model, layers, dataset, out_name, num_images_per_class, device):
+def create_feature_dataset(model, layers, out_dims, dataset, out_name, num_images_per_class, device):
     encoder = FeatureExtractor(model, layers)
     encoder.eval()
     
-    print(f'saving to {out_name}...')
-    preds = {layer : [] for layer in layers}
-
+    
+    preds = {layers[i] : np.zeros([num_images_per_class * 10] + out_dims[i], dtype=np.float32) for i in range(len(layers))}
     label_counts = {i:0 for i in range(10)}
+    label_not_finish = [True for _ in range(10)]
+
+    i = 0
     for image, label in tqdm(dataset):
-        if label_counts[label] > num_images_per_class:
+        if not any(label_not_finish):
+            print('all classes are computed')
+            break
+        if label_counts[label] >= num_images_per_class:
+            label_not_finish[label] = False
             continue
+
         features = encoder(torch.unsqueeze(image.to(device), dim=0))
         for layer in features.keys():
-            preds[layer].append(features[layer].detach().cpu())
+            preds[layer][i] = features[layer].detach().cpu().numpy()[0]
+        i+=1
         label_counts[label]+=1
-    
-    for layer in preds.keys():
-        preds[layer] = torch.cat(preds[layer]).numpy()
-
+    print(f'saving to {out_name}...')
     np.savez_compressed(out_name, **preds)
+
+
